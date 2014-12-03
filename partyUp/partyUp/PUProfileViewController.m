b0792d190685c6a56794855ea359658e86dcde9c
Thiago Lioy <lioy@Thiagos-MacBook-Air-2.local>
2014-12-03 13:03:30 -0200

    removed buddies from profile

--- partyUp/partyUp/PUProfileViewController.m
+++ partyUp/partyUp/PUProfileViewController.m
@@ -12,17 +12,10 @@
 #import "PUBestBuddyTableViewCell.h"
 #import "PUHeaderTableViewCell.h"
 
-@interface PUProfileViewController ()<UITableViewDataSource,UITableViewDelegate>
+@interface PUProfileViewController ()
 @property (strong, nonatomic) IBOutlet FBProfilePictureView *profilePicture;
 @property (strong, nonatomic) IBOutlet UILabel *profileName;
-@property (strong, nonatomic) IBOutlet UITableView *bestBuddiesTableView;
-@property (strong, nonatomic) NSArray *bestBuddies;
-
 @end
-
-
-static NSString *bestBuddiesCellID = @"BestBuddyCellID";
-static NSString *headerCellID = @"HeaderCellID";
 
 @implementation PUProfileViewController
 
@@ -35,7 +28,6 @@ static NSString *headerCellID = @"HeaderCellID";
 
 -(void)viewWillAppear:(BOOL)animated{
     self.parentViewController.navigationItem.title = @"Profile";
-    [self recoverBuddiesFromCache];
 }
 
 
@@ -49,10 +41,6 @@ static NSString *headerCellID = @"HeaderCellID";
     [_profilePicture roundIt:16.0f];
 }
 
--(void)recoverBuddiesFromCache{
-    _bestBuddies = [PUBuddiesStorage storedBuddies];
-    [_bestBuddiesTableView reloadData];
-}
 
 -(void)recoverUserFromCache{
     PUUser *me = [PUBuddiesStorage myself];
@@ -72,35 +60,5 @@ static NSString *headerCellID = @"HeaderCellID";
     if([vc isKindOfClass:[UINavigationController class]])
         [((UINavigationController*)vc) popToRootViewControllerAnimated:YES];
 }
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return _bestBuddies.count;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    PUBestBuddyTableViewCell *cell = (PUBestBuddyTableViewCell*)[tableView dequeueReusableCellWithIdentifier:bestBuddiesCellID];
-    
-    PUUser *buddy = [_bestBuddies objectAtIndex:indexPath.row];
-    [cell fill:buddy];
-    return cell;
-}
-
--(UIView*)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
-    PUHeaderTableViewCell *cell = (PUHeaderTableViewCell*)[tableView dequeueReusableCellWithIdentifier:headerCellID];
-    if([self hasBestBuddies:section])
-        cell.message.text = @"Best Buddies";
-    return [cell contentView];
-}
--(BOOL)hasBestBuddies:(NSInteger)section{
-    return  _bestBuddies.count > 0;
-}
-
-- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
-    if([self hasBestBuddies:section])
-        return 44.0f;
-    return 0.0f;
-}
-
-
 
 @end
